b3589945fe2a977b6780e01761e4154e0da0d8ef
Kevin Merinsky <kevinmerinsky@gmail.com>
2021-11-26 21:46:26 -0500

    Feat: Video component

--- site/src/pages/blender/001/index.jsx
+++ site/src/pages/blender/001/index.jsx
@@ -2,6 +2,7 @@ import styles from "./001.module.scss";
 import React, { useState } from "react";
 
 import Header from "@components/elements/Header/Header";
+import Video from "@components/elements/Video/Video";
 
 export default function Blender001() {
   const [animate, setAnimate] = useState(true);
@@ -9,11 +10,11 @@ export default function Blender001() {
     <>
       <Header
         title="Exploring Blender Physics"
-        info="This is an exploration of how to animate in Blender using physics."
+        info="This is an exploration of how to animate in Blender using physics. In this instance, the ball is manually keyframed up until it meets the tower of cubes. From that point on, Blender's physics engine takes over and animates the position of everything using properties like weight and friction that are assigned to each of the objects."
         recycle
       />
       <div className={styles.flex}>
-        <h1>Blender</h1>
+        <Video />
       </div>
     </>
   );
