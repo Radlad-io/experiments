e090cab6e7202763a95cc0ee39273c6dbed8068b
Kevin Merinsky <kevinmerinsky@gmail.com>
2021-12-01 09:29:19 -0500

    Content: CAD RadBooth

--- site/src/pages/cad/radbooth/index.js
+++ site/src/pages/cad/radbooth/index.js
@@ -5,16 +5,14 @@ export default function P5001() {
   return (
     <>
       <Header
-        title="3D Printer Design"
-        info="This was an open source FDM 3d printer design that I was working on for fun. At the time open source machines all seemed to prioritize low cost which presented quality issues. This machine was being designed as a middle ground between high end machines and low cost machines. A surge of new 3d printer companies caused me to halt this project. Valuable experience in CAD was gained."
+        title="RadBooth"
+        info="This was an open source Photo and Video booth I'm working on. The general idea is that it will utilize a mirrorless camera connected to a small single board computer to create a photo booth capable of creative images, GIFs, videos and slow motion videos."
         recycle
       />
       <iframe
         className={styles.frame}
-        src="https://myhub.autodesk360.com/ue29e8783/g/shares/SH7f1edQT22b515c761e7856c5f45f3ad7ab"
+        src="https://myhub.autodesk360.com/ue29e8783/g/shares/SH9285eQTcf875d3c53963fea12558efdd32"
       ></iframe>
     </>
   );
 }
-
-//https://a360.co/3xHTPX2
