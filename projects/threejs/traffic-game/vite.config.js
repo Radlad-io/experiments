835026ac5f56b58e75b541fe01a5572ef05f6a7d
Kevin Merinsky <kevinmerinsky@gmail.com>
2021-12-07 22:56:39 -0500

    Fix: Vite build issue

---
+++ projects/threejs/traffic-game/vite.config.js
@@ -0,0 +1,14 @@
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        car: resolve(__dirname, "helpers/car/index.html"),
+        spotlight: resolve(__dirname, "helpers/spotlight/index.html"),
+      },
+    },
+  },
+});
